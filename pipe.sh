d0b7de8a56f3cadf8418ceeb5c5d508d8e6ecd18
player <playerc@msn.cn>
2026-06-13 02:10:08 +0800

    add .config for clangd

--- pipe.sh
+++ pipe.sh
@@ -2,11 +2,16 @@
 TDIR=~/.emacs.d/
 SDIR=./src/
 
+DCDIR=./src.config
+TDCDIR=~/.config
+
 cp ${SDIR}init.el ${TDIR} \
     && cp ${SDIR}clang-format.yaml ${TDIR} \
     && cp ${SDIR}clangd.yaml ${TDIR} \
-    &&  echo [OK]
+    &&  echo copy init.el [OK]
 
-cp -rf ${SDIR}themes ${TDIR} && echo [OK]
+cp -rf ${SDIR}themes ${TDIR} && echo copy themes [OK]
+
+cp -rf ${DCDIR} ${TDCDIR} && echo copy .config dir [OK]
 
 ##:~ EOF
